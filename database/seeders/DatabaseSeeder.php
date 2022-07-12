28f19877bf442b9b558ffac06f04fdf0fcde6aa8
Lucas Santana <lucas.fullstack.dev@gmail.com>
2022-07-12 02:21:44 -0300

    feat: UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserSeeder::class,
             CategoriaSeeder::class,
             ProdutoSeeder::class
         ]);
